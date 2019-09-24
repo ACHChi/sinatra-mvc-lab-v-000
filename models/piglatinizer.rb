c0874883a8fd54816ae906fa2642feb5577e37d7
Chao Hsuan Chi <allen0413@gmail.com>
2019-09-24 21:29:41 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,14 @@
 class PigLatinizer
-  
+
   def piglatinize(input_str)
      input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
    end
 
- 
+
    def consonant?(char)
      !char.match(/[aAeEiIoOuU]/)
    end
- 
+
    def piglatinize_word(word)
      # word starts with vowel
      if !consonant?(word[0])
@@ -25,9 +25,9 @@ class PigLatinizer
      end
      word << "ay"
    end
- 
+
    def piglatinize_sentence(sentence)
      sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
    end
-   
+
 end
